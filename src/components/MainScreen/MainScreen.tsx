9d95cf2c10afc3d9fe47c75e8605e2dadab4e5f9
phi1goodinc <goodphilinc@gmail.com>
2023-04-27 20:15:55 +0300

    Static Main screen added.

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,11 +1,24 @@
 import {FC} from "react";
 import css from './MainScreen.module.css'
+import powerButton from '../../assets/images/power-button.svg'
+import bluePlanet from '../../assets/images/blue-planet.svg'
 
 
 const MainScreen: FC = () => {
     return (
         <section className={css.main}>
-            <h3>Buy Premium</h3>
+            <div className={css.connection_section}>
+                <div className={`${css.connection_button}`}>
+                    <div className={css.loader}></div>
+                    <button>
+                        <img src={powerButton} alt="power button"/>
+                    </button>
+                </div>
+                <p className={css.connection_status}>Press to connect</p>
+            </div>
+            <div className={'css.connection_planet'}>
+                <img src={bluePlanet} alt=""/>
+            </div>
         </section>
 
     )
